44006ba419f9282ad0a3aedcaeb56f9138ac46a8
hirokihello <iammyeye1@gmail.com>
2019-11-24 20:44:06 +0900

    cloudfrontの削除

--- cloudfront.tf
+++ cloudfront.tf
@@ -6,63 +6,63 @@ resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
   comment = "origin_access_identity"
 }
 
-resource "aws_cloudfront_distribution" "s3_distribution" {
+# resource "aws_cloudfront_distribution" "s3_distribution" {
 
-  origin {
-    domain_name = "${aws_s3_bucket.hirokihello_portfolio.bucket_regional_domain_name}"
-    origin_id   = "${local.s3_origin_id}"
+#   origin {
+#     domain_name = "${aws_s3_bucket.hirokihello_portfolio.bucket_regional_domain_name}"
+#     origin_id   = "${local.s3_origin_id}"
 
-    s3_origin_config {
-      origin_access_identity = "${aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path}"
-    }
-  }
+#     s3_origin_config {
+#       origin_access_identity = "${aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path}"
+#     }
+#   }
 
-  aliases = ["hirokihello.xyz",]
-  enabled             = true
-  is_ipv6_enabled     = true
-  comment             = "comment"
-  default_root_object = "index.html"
+#   aliases = ["hirokihello.xyz",]
+#   enabled             = true
+#   is_ipv6_enabled     = true
+#   comment             = "comment"
+#   default_root_object = "index.html"
 
-  logging_config {
-    include_cookies = false
-    bucket          = "alb-log-hirokihello-portfolio.s3.amazonaws.com"
-    prefix          = "production-log"
-  }
+#   logging_config {
+#     include_cookies = false
+#     bucket          = "alb-log-hirokihello-portfolio.s3.amazonaws.com"
+#     prefix          = "production-log"
+#   }
 
-  default_cache_behavior {
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "${local.s3_origin_id}"
+#   default_cache_behavior {
+#     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+#     cached_methods   = ["GET", "HEAD"]
+#     target_origin_id = "${local.s3_origin_id}"
 
-    forwarded_values {
-      query_string = false
+#     forwarded_values {
+#       query_string = false
 
-      cookies {
-        forward = "none"
-      }
-    }
+#       cookies {
+#         forward = "none"
+#       }
+#     }
 
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
+#     viewer_protocol_policy = "redirect-to-https"
+#     min_ttl                = 0
+#     default_ttl            = 3600
+#     max_ttl                = 86400
+#   }
 
-  price_class = "PriceClass_200"
+#   price_class = "PriceClass_200"
 
-#あとで必要になったら
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
+# #あとで必要になったら
+#   restrictions {
+#     geo_restriction {
+#       restriction_type = "none"
+#     }
+#   }
 
-  tags = {
-    Environment = "production"
-  }
+#   tags = {
+#     Environment = "production"
+#   }
 
-  viewer_certificate {
-    acm_certificate_arn = "${aws_acm_certificate.cert.arn}"
-    ssl_support_method  = "sni-only"
-  }
-}
+#   viewer_certificate {
+#     acm_certificate_arn = "${aws_acm_certificate.cert.arn}"
+#     ssl_support_method  = "sni-only"
+#   }
+# }
