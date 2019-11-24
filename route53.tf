97c0df56256059807530bd3231f8cb6398708257
hirokihello <iammyeye1@gmail.com>
2019-11-24 21:07:34 +0900

    cloudfrontの部分のコメントアウトの解除

--- route53.tf
+++ route53.tf
@@ -2,14 +2,14 @@ resource "aws_route53_zone" "hirokihello_portfolio_public" {
   name = "hirokihello.xyz"
 }
 
-# resource "aws_route53_record" "hirokihello_portfolio_routing" {
-#   zone_id = "${aws_route53_zone.hirokihello_portfolio_public.zone_id}"
-#   name = "hirokihello.xyz"
-#   type = "A"
+resource "aws_route53_record" "hirokihello_portfolio_routing" {
+  zone_id = "${aws_route53_zone.hirokihello_portfolio_public.zone_id}"
+  name = "hirokihello.xyz"
+  type = "A"
 
-#   alias {
-#     name = "${aws_cloudfront_distribution.s3_distribution.domain_name}"
-#     zone_id = "${aws_cloudfront_distribution.s3_distribution.hosted_zone_id}"
-#     evaluate_target_health = false
-#   }
-# }
+  alias {
+    name = "${aws_cloudfront_distribution.s3_distribution.domain_name}"
+    zone_id = "${aws_cloudfront_distribution.s3_distribution.hosted_zone_id}"
+    evaluate_target_health = false
+  }
+}
